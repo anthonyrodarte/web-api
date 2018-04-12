b1984ea46a84650125b4491c080d4d89b933e493
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-04-12 11:04:50 -0700

    A user can read a list of notes. closes #3

--- index.js
+++ index.js
@@ -31,4 +31,25 @@ app.post('/note', (req, res) => {
   })
 })
 
+app.get('/note', (req, res) => {
+  MongoClient.connect('mongodb://localhost/library', (err, client) => {
+    if (err) {
+      console.err(err)
+      res.sendStatus(500)
+      process.exit(1)
+    }
+    const db = client.db('library')
+    const notebook = db.collection('notebook')
+
+    notebook.find().toArray((err, result) => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log(result)
+      }
+      client.close()
+    })
+    res.sendStatus(200)
+  })
+})
 app.listen(3000)
