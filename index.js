9da0db16f4a7aef0f429d1aad05fc69beccdd4fb
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-04-12 10:02:13 -0700

    A user can save a note. closes #1

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const { MongoClient } = require('mongodb')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log('listening on 3000!')
+const jsonParser = bodyParser.json()
+
+app.use(jsonParser)
+app.post('/note', (req, res) => {
+  MongoClient.connect('mongodb://localhost/library', (err, client) => {
+    if (err) {
+      console.error(err)
+      res.sendStatus(500)
+      process.exit(1)
+    }
+
+    const db = client.db('library')
+    const notebook = db.collection('notebook')
+
+    notebook.insertOne(req.body)
+
+    res.sendStatus(201)
+  })
 })
+
+app.listen(3000)
